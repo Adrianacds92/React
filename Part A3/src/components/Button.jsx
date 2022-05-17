ee0d68d5a23cd35ebd1fbd48c7728d7fab89e365
Adrianacds92 <91399215+Adrianacds92@users.noreply.github.com>
2022-05-17 13:51:29 -0300

    Delete Part A3/src directory

--- Part A3/src/components/Button.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function Button(props) {
-  console.log(props);
-  return <button className={`btn first ${props.className}`}>{props.innerText}</button>;
-}
-
-export default Button;
