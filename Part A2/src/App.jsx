4340532ffeba17f2ea67109d10526062349538d6
Adrianacds92 <91399215+Adrianacds92@users.noreply.github.com>
2022-05-17 13:56:55 -0300

    Update App.jsx

--- Part A2/src/App.jsx
+++ Part A2/src/App.jsx
@@ -5,15 +5,15 @@ import { FaUserAlt } from 'react-icons/fa';
 function App() {
   return (
     <div>
-      <header className='titlle'>
+      <header className='title'>
         <div className='quadrado'></div>
       <p className='button'><span className='btn1'>BTN1</span></p>
       <p className='btn2'>BTN2</p>
       <p className='btn3'>BTN3</p>
       <p className='btn4'>BTN4</p>
       </header>
-      <div className='retangulo'> <span className='icon'><FaUserAlt /></span></div>
-      <div className='circulo'></div>
+      <div className='area'> <span className='icon'><FaUserAlt /></span></div>
+      <div className='round'></div>
     </div>
   );
 }
